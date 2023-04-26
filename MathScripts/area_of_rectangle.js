c78f6fd948caa4e983ecb6b18d622c4e6deaa381
Regin Guevarra <130509430+ReginGuevarra@users.noreply.github.com>
2023-04-26 13:51:35 -0700

    Update area_of_rectangle.js

--- MathScripts/area_of_rectangle.js
+++ MathScripts/area_of_rectangle.js
@@ -1,12 +1,18 @@
-// Function to calculate the area of a rectangle
-function calculateRectangleArea(length, width) {
-    return length * width;
-  }
-  
-  // Example usage
-  let length = 10;
-  let width = 5;
-  
-  let area = calculateRectangleArea(length, width);
-  console.log("The area of the rectangle is:", area);
-  
+//input
+let length = 10;
+let width = 20;
+let surface = length * width // 200
+let amountFeetPerBox = 12;
+
+
+//Algorithm // Can't be half and should add 10% rest boxes
+let amountOfBoxes = (totalSurfaceFeet / amountFeetPerBox) * 1.1;
+
+//Calculate 10% extra
+
+console.log("Amount of boxes; " + Math.ceil(amountOfBoxes));
+
+
+
+
+//output
