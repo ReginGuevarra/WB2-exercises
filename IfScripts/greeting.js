47a14225f5581c741fa7540fe447f5b02ffa6d0b
Regin Guevarra <130509430+ReginGuevarra@users.noreply.github.com>
2023-04-26 16:04:11 -0700

    greeting.js savepoint

---
+++ IfScripts/greeting.js
@@ -0,0 +1,11 @@
+// get current hour
+let currentHour = new Date().getHours();
+
+// display greeting based on current hour
+if (currentHour < 10) {
+  console.log("Good morning!");
+} else if (currentHour < 18) {
+  console.log("Good afternoon!");
+} else {
+  console.log("Good evening!");
+}
